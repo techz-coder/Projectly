e9543a2e4811b6acbe7933e1bcc416a8413b23de
Sahil <127021097+techz-coder@users.noreply.github.com>
2024-05-28 14:27:27 +0530

    loginQuery.php

--- loginQuery.php
+++ loginQuery.php
@@ -12,8 +12,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (mysqli_num_rows($result) == 1) {
         $row = mysqli_fetch_assoc($result);
         $_SESSION['name'] = $row['name'];
-        $_SESSION['mobile'] = $row['mobile'];
+        $_SESSION['workspace'] =  $row['workspace'];
         $_SESSION['email'] = $row['email'];
+        $_SESSION['address'] =  $row['address'];
+        $_SESSION['city'] =  $row['city'];
+        $_SESSION['state'] =  $row['state'];
+        $_SESSION['pin'] =  $row['pin'];
+        $_SESSION['country'] =  $row['country'];
+        $_SESSION['mobile'] =  $row['mobile'];
+        $_SESSION['password'] =  $row['password'];
+        $_SESSION['cpassword'] =  $row['cpassword'];
+
         header("Location: index.php");
         exit();
     } else {
