3a582d14845afa304b0d80d63aab76c418693a97
Sahil <127021097+techz-coder@users.noreply.github.com>
2024-05-28 14:25:05 +0530

    Update new_user.sql

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 28, 2024 at 09:25 AM
+-- Generation Time: May 28, 2024 at 10:52 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -24,47 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `new_user`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `new_user` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `mobile` varchar(255) DEFAULT NULL,
+  `workspace` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `pin` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `cpassword` varchar(255) DEFAULT NULL,
+  `terms` varchar(255) DEFAULT NULL,
+  `subscribes` varchar(255) DEFAULT NULL,
   `created_at` datetime(6) NOT NULL DEFAULT current_timestamp(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `new_user`
 --
 
-INSERT INTO `users` (`id`, `name`, `mobile`, `email`, `password`, `created_at`) VALUES
-(1, '', '', '', '', '2024-05-25 02:02:17.097518'),
-(2, 'asffasfasd', '8976898789', 'asfas@gmail.com', 'fasfa', '2024-05-25 02:10:17.452131'),
-(3, 'asffasfasd', '8976898789', 'asfas@gmail.com', 'fasfa', '2024-05-25 02:11:09.119020'),
-(4, 'asffasfasd', '8976898789', 'asfas@gmail.com', 'fasfa', '2024-05-25 02:11:25.544566'),
-(5, 'asffasfasd', '8976898789', 'asfas@gmail.com', 'asfasdfasd', '2024-05-25 02:13:29.409736'),
-(6, 'sdfasd', '78688787768', 'fasdfasdf@gmail.com', 'fsdfasdjfasd', '2024-05-25 02:15:43.832671'),
-(7, 'sdfasd', '78688787768', 'fasdfasdf@gmail.com', 'fsdfasdjfasd', '2024-05-25 02:15:51.093651'),
-(8, 'sahil', '9867354678', 'sahil@gmail.com', 'sahil@123', '2024-05-27 13:18:05.082234'),
-(9, 'sahil', '9867354678', 'sahil@gmail.com', 'sahil@123', '2024-05-27 13:18:22.327242'),
-(10, 'sahil', '9867354678', 'sahil@gmail.com', 'sahil@123', '2024-05-27 13:18:50.704661'),
-(11, 'hello ', '885786867', 'mahesh@gmail.com', 'mahesh', '2024-05-27 13:19:02.237744'),
-(12, 'sahil', '8750974176', 'sahil@gmail.com', 'sahil@123', '2024-05-27 15:25:58.514120'),
-(13, 'sanjay', '97865346789', 'sanjay@gmail.com', 'sanjay@!23', '2024-05-27 17:30:58.449751'),
-(14, 'sahil', '7889789899', 'admin@gmail.com', 'admin@123', '2024-05-28 11:58:27.387375'),
-(15, 'ramesh', '789978798789', 'ramesh@gmail.com', 'ramesh@123', '2024-05-28 12:29:11.701234');
+INSERT INTO `new_user` (`id`, `name`, `workspace`, `email`, `address`, `city`, `state`, `pin`, `country`, `mobile`, `password`, `cpassword`, `terms`, `subscribes`, `created_at`) VALUES
+(1, 'gsdtsdg', 'hjk', 'admin@gmail.com', 'jkh', 'jkhjkh', 'jkh', '68465456', 'h', '4424545', 'admin@123', 'hjk', 'on', 'on', '2024-05-28 13:58:29.995293');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users`
+-- Indexes for table `new_user`
 --
-ALTER TABLE `users`
+ALTER TABLE `new_user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -72,10 +67,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `new_user`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+ALTER TABLE `new_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
