e1aec763ab3e43449ee26c3f8b0ff712d25578d8
Sahil <127021097+techz-coder@users.noreply.github.com>
2024-05-28 14:26:19 +0530

    Update register.php

--- register.php
+++ register.php
@@ -48,29 +48,84 @@ include "inc/header.php";
             <form id="register_form" method="post" action="insertQuery.php">
                 <div class="row g-3">
                     <div class="col-md-6">
-                        <label for="your-email" class="form-label">Your Name</label>
+                        <label for="name" class="form-label">Your Name</label>
                         <input type="text" class="form-control" id="name" name="name" required placeholder="Name">
                     </div>
                     <div class="col-md-6">
-                        <label for="your-name" class="form-label">Mobile</label>
-                        <input type="number" class="form-control" id="mobile" name="mobile" required
-                            placeholder="1234567890">
+                        <label for="workspace-name" class="form-label">Workspace Name</label>
+                        <input type="text" class="form-control" id="workspace-name" name="workspace" required
+                            placeholder="Workspace Name">
                     </div>
                     <div class="col-md-6">
-                        <label for="your-email" class="form-label">Your Email</label>
+                        <label for="email" class="form-label">Your Email</label>
                         <input type="email" class="form-control" id="email" name="email" required
                             placeholder="example@gmail.com">
                     </div>
                     <div class="col-md-6">
-                        <label for="your-name" class="form-label">Password</label>
+                        <label for="address" class="form-label">Address</label>
+                        <input type="text" class="form-control" id="address" name="address" required
+                            placeholder="Enter a location">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="city" class="form-label">City</label>
+                        <input type="text" class="form-control" id="city" name="city" required placeholder="City">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="state" class="form-label">State</label>
+                        <input type="text" class="form-control" id="state" name="state" required placeholder="state">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="pin" class="form-label">Pincode/Zipcode</label>
+                        <input type="number" class="form-control" id="pin" name="pin" required placeholder="XXXXXX">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="country" class="form-label">Country</label>
+                        <input type="text" class="form-control" id="country" name="country" required
+                            placeholder="India">
+                    </div>
+                    <!-- <div class="col-md-6">
+                        <label for="mobile" class="form-label">Contact Number</label>
+                        <input type="number" class="form-control" id="mobile" name="mobile" required
+                            placeholder="0123456789">
+                    </div> -->
+
+
+                    <div class="col-md-6">
+                        <label class="sr-only" for="inlineFormInputGroup">Contact Number</label>
+                        <div class="input-group mt-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">+91</div>
+                            </div>
+                            <input type="number" class="form-control" id="inlineFormInputGroup" name="mobile" required>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6">
+                        <label for="password" class="form-label">Password</label>
                         <input type="password" class="form-control" id="password" name="password" required
                             placeholder="xxxxxxxxxxx">
                     </div>
+                    <div class="col-md-6">
+                        <label for="password" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" id="password" name="cpassword" required
+                            placeholder="xxxxxxxxxxx">
+                    </div>
+                    <div class="col-md-12">
+                        <label for="terms" class="form-label">Terms and conditions</label>
+                        <input type="checkbox" id="password" name="terms" required>
+                    </div>
+                    <div class="col-md-12">
+                        <label for="subscribe" class="form-label">Subscriber to newsletter</label>
+                        <input type="checkbox" id="password" name="subscribe" required>
+                    </div>
+
 
                     <div class="col-12">
                         <div class="row">
                             <div class="col-md-6">
-                                <input type="submit" value="Register" name="register_btn" id="register" class="btn btn-dark w-100 fw-bold">
+                                <input type="submit" value="Register" name="register_btn" id="register"
+                                    class="btn btn-dark w-100 fw-bold">
                             </div>
                         </div>
                         <h6><a href="login.php" style=" margin:1rem; text-decoration: none;">Already have an
